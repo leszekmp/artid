05918866459045708a5ba60aaf2d2e1a787586cd
Jean-Roc Morreale <jr.morreale@enoreth.net>
2017-03-02 16:44:11 +0100

    set pdf paper size to A4

--- conf.py
+++ conf.py
@@ -89,7 +89,7 @@ htmlhelp_basename = 'manual_affordable_rti'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
